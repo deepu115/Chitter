27e2a2ce93e895f2d6710875ea87ece60e56e016
Dileep <dileepvunnam@gmail.com>
2023-08-12 17:04:14 +0100

    authorization controller

---
+++ ChitterBackEnd/controllers/authController.js
@@ -0,0 +1,74 @@
+import User from '../models/user.js';
+import { hashPassword, comparePassword, generateToken } from '../services/authService.js';
+import { body, validationResult } from 'express-validator';
+
+// User Registration/Signup
+export const signup = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const { username, email, password } = req.body;
+
+        // Check if user already exists
+        let user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ msg: 'User already exists' });
+        }
+
+        // Hash password
+        const hashedPassword = await hashPassword(password);
+
+        // Create user in database
+        user = new User({
+            username,
+            email,
+            password: hashedPassword
+        });
+        await user.save();
+
+        // Generate JWT token
+        const token = generateToken(user);
+
+        res.json({ token });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+};
+
+// User Login
+export const login = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const { email, password } = req.body;
+
+        // Check if user exists
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ msg: 'Invalid credentials' });
+        }
+
+        // Compare passwords
+        const isMatch = await comparePassword(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ msg: 'Invalid credentials' });
+        }
+
+        // Generate JWT token
+        const token = generateToken(user);
+
+        res.json({ token });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+};
+
+
