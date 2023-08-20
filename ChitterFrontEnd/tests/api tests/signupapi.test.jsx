61739aecdc6573248be4f67030c4e07ceb671c82
Dileep <dileepvunnam@gmail.com>
2023-08-20 16:59:41 +0100

    some signup tests passed

---
+++ ChitterFrontEnd/tests/api tests/signupapi.test.jsx
@@ -0,0 +1,70 @@
+import axios from 'axios';
+import { render, screen, fireEvent } from '@testing-library/react';
+import Signup from '../../src/Components/Signup';
+import { MemoryRouter as Router } from 'react-router-dom';
+import { act } from 'react-dom/test-utils';
+
+vi.mock('axios');
+
+describe('Signup Component', () => {
+    afterEach(() => {
+        vi.clearAllMocks();
+    });
+    it('should make a successful API call on form submission', async () => {
+        axios.post.mockResolvedValueOnce({ data: {} });
+
+        render(
+            <Router>
+                <Signup />
+            </Router>
+        );
+
+        fireEvent.change(screen.getAllByLabelText(/name/i)[0], { target: { value: 'Harry Potter' } });
+        fireEvent.change(screen.getByLabelText(/username/i), { target: { value: 'harry' } });
+        fireEvent.change(screen.getByLabelText(/email/i), { target: { value: 'hpotter@gmail.com' } });
+        fireEvent.change(screen.getByLabelText(/password/i), { target: { value: '12345678' } });
+        await act(async () => {
+            fireEvent.click(screen.getByRole('button', { name: /signup/i }));
+        });
+
+        expect(axios.post).toHaveBeenCalledWith('http://localhost:3000/api/users/signup', {
+            name: 'Harry Potter',
+            username: 'harry',
+            email: 'hpotter@gmail.com',
+            password: '12345678'
+        }, expect.anything());
+    });
+    it('should display an error message on failed API call', async () => {
+        const mockError = "Signup failed!";
+        axios.post.mockRejectedValueOnce({
+            response: {
+                data: {
+                    msg: mockError
+                }
+            }
+        });
+
+        render(
+            <Router>
+                <Signup />
+            </Router>
+        );
+        fireEvent.change(screen.getAllByLabelText(/name/i)[0], { target: { value: 'Harry Potter' } });
+        fireEvent.change(screen.getByLabelText(/username/i), { target: { value: 'harry' } });
+        fireEvent.change(screen.getByLabelText(/email/i), { target: { value: 'hpotter@gmail.com' } });
+        fireEvent.change(screen.getByLabelText(/password/i), { target: { value: '12345678' } });
+        fireEvent.click(screen.getByRole('button', { name: /signup/i }));
+        expect(await screen.findByText(mockError)).toBeInTheDocument();
+    });
+    it('should not make an API call if the form is submitted with empty fields', async () => {
+        render(
+            <Router>
+                <Signup />
+            </Router>
+        );
+        fireEvent.click(screen.getByRole('button', { name: /signup/i }));
+        expect(axios.post).not.toHaveBeenCalled();
+    });
+
+
+});
